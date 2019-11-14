68c264a091a200868fc80d650bf0fbcdc5d1078b
baudon <olivier.baudon@labri.fr>
2019-11-14 09:18:44 +0100

    Update CarTest.java

--- test/fr/ubordeaux/ao/domain/model/CarTest.java
+++ test/fr/ubordeaux/ao/domain/model/CarTest.java
@@ -11,7 +11,8 @@ public class CarTest {
 
 	Make citroen = new Make("Citroen");
 	Model c2 = new Model("C2", citroen);
-	
+	Model c3 = new Model("C3", citroen);
+
 	@Before
 	public void setUp() throws Exception {
 		
@@ -20,14 +21,26 @@ public class CarTest {
 	@Test(expected = EntryException.class)
 	public final void testCar() {
 		Car ca = new Car(1, c2);
-		Car cb = new Car(1, c2);
+		Car cb = new Car(1, c3);
 	}
+	
+	@Test
+	public final void testCar2() {
+		Set<Car> cars = new HashSet<Car>();
+		Car ca = new Car(1, c2);
+		Car cb = new Car(1, c2);
+		cars.add(ca);
+		cars.add(cb);
+		assertEquals(cars.size(), 1);
+	}
+
+
 	
 	public final void testId() {
 		Car c = new Car(1, c2);
 		assertEquals(c.id(), 1);
 	}
-	
+		
 	public final void testModel() {
 		Car c = new Car(1, c2);
 		assertEquals(c.model(), c2);
