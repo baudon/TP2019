34242fcfe6cb6c798546cc7575850f907427f3c7
baudon <olivier.baudon@labri.fr>
2019-12-03 22:15:52 +0100

    Add files via upload

---
+++ src/fr/ubordeaux/ao/domain/model/Model.java
@@ -0,0 +1,33 @@
+package fr.ubordeaux.ao.domain.model;
+
+import java.util.Objects;
+
+public class Model {
+
+	private String name;
+	private Constructor constructor;
+	
+	public Model(String name, Constructor constructor) {
+		this.name = name;
+		this.constructor = constructor;
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public Constructor constructor() {
+		return constructor;
+	}
+	
+	public boolean equals(Object o) {
+		if (! (o instanceof Model)) 
+			return false;
+		Model m = (Model) o;
+		return m.name.equals(name) && m.constructor.equals(constructor);
+	}
+	
+	public int hashCode() {
+		return Objects.hash(name, constructor);
+	}
+}
